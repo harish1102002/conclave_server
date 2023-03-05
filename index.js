61764cab49befe0bf6f4ada909ee36972997a097
HARISH R P <harishpranesh983@gmail.com>
2023-03-05 12:44:39 +0530

    hi

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ col=c.db().collection("conclave")).then(()=>{
 
 const io = new Server(http,{
     cors:{
-        origin:"https://conclavee.netlify.app"
+        origin:"https://conclavee.netlify.app/"
     },
 })
 
