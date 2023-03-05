fd8f55fbfa0bb8e0244b077e2f283d1584b098f8
HARISH R P <harishpranesh983@gmail.com>
2023-03-05 09:29:22 +0530

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+const  exp = require("express");
+const app = exp();
+const cors= require("cors");
+const http=require('http').createServer(app);
+const {Server}=require('socket.io');
+const m = require('mongodb').MongoClient;
+
+let post=[]
+let col=null
+app.use(exp.json())
+app.use(cors())
+
+m.connect("mongodb+srv://Harish:harish777@cluster0.ffgjypz.mongodb.net/Kuruk").then((c)=>
+col=c.db().collection("conclave")).then(()=>{
+    col.find().forEach(e => post.push(e));});
+
+const io = new Server(http,{
+    cors:{
+        origin:"http://localhost:3000"
+    },
+})
+
+io.on("connection",(soc)=>{
+    soc.on("send",(data)=>{
+        col.insertOne(data);
+        post.push(data);
+        io.emit("receive",data);
+    })
+})
+
+app.get('/',(req,res)=>res.json(post))
+http.listen(process.env.PORT||8000)
